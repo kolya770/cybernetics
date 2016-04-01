3f45beff970715f3716e30ac1c39c0f72cdb47ab
kolya770 <grinko.kolya770@gmail.com>
2016-04-01 07:56:26 +0300

    1

--- js/main.js
+++ js/main.js
@@ -30,13 +30,42 @@ $(document).ready(function() {
     });
 
     // Carousel mini
-
-    $('.slider4').bxSlider({
-        slideWidth: 350,
-        minSlides: 2,
-        maxSlides: 3,
-        moveSlides: 1,
-        slideMargin: 10
+    $('.slider-mini').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        prevArrow:"<img class='a-left control-c prev slick-prev' src='img/Shape-left.png'>",
+        nextArrow:"<img class='a-right control-c next slick-next' src='img/Shape-right.png'>",
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 
 });
